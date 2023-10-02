7d8af89ef7d57742fa10cee9dfdc44a6fd3e536c
maurojh <maurojh@gmail.com>
2023-10-02 06:27:37 -0300

    Tema escuro

--- lib/pages/configuracoes_page.dart
+++ lib/pages/configuracoes_page.dart
@@ -11,25 +11,37 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
   String? nomeUsuario;
   double? altura;
   bool receberPushNotification = false;
-  bool temaEscura = false;
+  bool temaEscuro = false;
+  TextEditingController controlaNome = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Configurações'),
+          title: const Text('Configurações'),
         ),
         body: Container(
           child: ListView(
             children: [
+              TextField(
+                controller: controlaNome,
+                
+              ),
               SwitchListTile(
-                title: Text('Receber notificações:'),
+                title: const Text('Receber notificações:'),
                 value: receberPushNotification, onChanged: (bool valor) {
                 setState(() {
                   receberPushNotification = valor;
                 });
-              },)
+              },),
+              SwitchListTile(
+                title: const Text('Tema escuro:'),
+                value: temaEscuro, onChanged: (bool valor) {
+                setState(() {
+                  temaEscuro = valor;
+                });
+              },),
             ],
           ),
         ),
