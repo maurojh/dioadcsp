fca1b9a02a08d22cdd9bc58fcfb5a8d2e6322750
maurojh <maurojh@gmail.com>
2023-10-02 06:05:44 -0300

    Pagina configuracoes

--- lib/pages/numeros_aleatorios_page.dart
+++ lib/pages/numeros_aleatorios_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NumerosAleatoriosPage extends StatefulWidget {
   const NumerosAleatoriosPage({super.key});
@@ -13,6 +14,25 @@ class _NumerosAleatoriosPageState extends State<NumerosAleatoriosPage> {
   int numeroGerado = 0;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    carregarDados();
+  }
+
+  void carregarDados() async {
+    final armazenamento = await SharedPreferences.getInstance();
+    var retorno = armazenamento.getInt('aleatorio');
+    setState(() {
+      if (retorno == null) {
+        numeroGerado = 0;
+      } else {
+        numeroGerado = retorno;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -21,20 +41,19 @@ class _NumerosAleatoriosPageState extends State<NumerosAleatoriosPage> {
         ),
         body: Container(
           child: Column(
-            children: [
-              Text('$numeroGerado')
-            ],
+            children: [Text('$numeroGerado')],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            var num = Random().nextInt(1000);
-            setState(() {
-              numeroGerado = num;
-            });
-          }
-        ),
+            child: const Icon(Icons.add),
+            onPressed: () async {
+              final armazenamento = await SharedPreferences.getInstance();
+              var num = Random().nextInt(1000);
+              setState(() {
+                numeroGerado = num;
+              });
+              armazenamento.setInt('aleatorio', numeroGerado);
+            }),
       ),
     );
   }
